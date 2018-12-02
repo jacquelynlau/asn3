d7db5ab4d622b1eae07a884a60db051370de4b2f
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-02 03:13:39 -0500

    edit

--- reorderproducts.php
+++ reorderproducts.php
@@ -5,7 +5,7 @@
   include 'connecttodb.php';
 ?>
 
-<form method="post" action="reorderproducts.php">
+<form method="post" action="assign3.php">
 <!-- organize by ascending or descending order button -->
 <select name = "order">
   <option selected = "selected" value = "ASC"> Ascending </option>
