14cc0a9ed16133571434574ad74f5926e0436c6c
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-02 13:43:33 -0500

    edit

--- getproducts1.php
+++ getproducts1.php
@@ -15,17 +15,17 @@
 		$whichCustomer = $_POST["customer"];
 		$query = 'SELECT * FROM Purchases, Product WHERE Product.customerID = Purchases.customerID AND Product.customerID="'.$whichCustomer.'"';
 
-
-
 		$result = mysqli_query($connection,$query);
-		if (!$result) {
-			die("database query2 failed.");
-		}
-
 		while ($row=mysqli_fetch_assoc($result)) {
 			echo '<li>';
 			echo $row["description"];
 			echo '</li>';
+		}
+
+
+
+		if (!$result) {
+			die("database query2 failed.");
 		}
 		mysqli_free_result($result);
 	}
