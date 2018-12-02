d4573b30719c111adbc41e40851ce52fbd986498
root <root@cs3319-vm108.novalocal>
2018-12-02 05:24:07 +0000

    first commit

---
+++ assign3.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<title> Store Database </title> 
+	<link rel="stylesheet" type="text/css" href="style.css"> 
+	<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
+</head> 
+<body> 
+
+<?php
+	include "connecttodb.php"; 
+?> 
+
+<h1> Assignment 3 Database </h1> 
+
+
+</div> 
+
+<div class ="main"> 
+
+</div>
+
+
+
+<?php
+	include "getcustomers.php";
+?> 
+
+  <form action = "getproducts.php" method = "post">
+
+	<input type = "submit" value = "Get Purchases">
+
+  </form> 
+
+
+</body> 
+</html> 
---
+++ getcustomers.php
@@ -0,0 +1,40 @@
+<?php 
+  	$query = "SELECT * FROM Customer GROUP BY lastName;";
+  	$result = mysqli_query($connection,$query);
+ 	if (!$result) {
+        	die("databases query failed.");	
+ 	}
+
+	echo "Who are you looking up? </br>";	
+	echo "<table border = '1'>
+	<tr> 
+	<th> Select: </th>
+	<th> Cusomter ID </th>  
+	<th> First Name </th>
+	<th> Last Name </th> 
+	<th> Phone Number </th> 
+	<th> City </th> 
+	<th> Agent ID </th>
+	</tr>";
+
+	while ($row = mysqli_fetch_assoc($result)) { 
+		echo '<tr><td><input type = "radio" name = "customer" value = "';
+		echo "<td>".$row ["customerID"]."</td>"; 
+		echo '"></td><td>'. $row["customerID"] . "</td>";
+		echo "<td>" . $row["firstName"] . "</td>";
+		echo "<td>" . $row["lastName"] . "</td>";
+		echo "<td>" . $row["phoneNumber"] . "</td>";
+		echo "<td>" . $row["city"] . "</td>";
+		echo "<td>" . $row["agentID"] . "</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
+
+	mysqli_free_result($result); 
+?>
+
+
+
+
+
+
