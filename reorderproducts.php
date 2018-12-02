f73d9135d45ac102a2765587ca7c226db5d8a4e0
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-02 03:03:56 -0500

    edit

--- reorderproducts.php
+++ reorderproducts.php
@@ -8,20 +8,21 @@
 <form method="post" action="assign3.php">
 <!-- organize by ascending or descending order button -->
 <select name = "order">
-  <option value = "ASC"> Ascending </option>
+  <option selected = "selected" value = "ASC"> Ascending </option>
   <option value = "DESC"> Descending </option>
 </select>
 
 <!-- organize by price or name button -->
 <select name = "type">
-  <option value = "price"> Price </option>
+  <option selected = "selected" value = "price"> Price </option>
   <option value = "name"> Name </option>
 </select>
 
 <!-- reorder the products button -->
 <input name = "submit" type = "submit" value = "Reorder Table"/>
-</form>
+
 
 <?php
   include 'getproducts2.php';
- ?>
+?>
+</form>
