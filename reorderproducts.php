d9da1ed42fa1b0ef3a77986fd3905190f7fc8aec
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-02 01:00:54 -0500

    MORE EDITS

--- reorderproducts.php
+++ reorderproducts.php
@@ -5,20 +5,20 @@
   include 'connecttodb.php';
 ?>
 
-// organize by ascending or descending order
+<!-- organize by ascending or descending order -->
 <select name = "order">
   <option selected = "select" values = "ascending"> Ascending </option>
   <option value = "descending"> Descending </option>
 </select>
 
-// organize by price or name
+<!-- organize by price or name -->
 <select name = "type">
   <option selected = "select" value = "price"> Price </option>
   <option value = "name"> Name </option>
 </select>
 
-//reorder the products
-<input type = "submit" value = "Reorder Table"/> 
+<!-- reorder the products -->
+<input type = "submit" value = "Reorder Table"/>
 
 
 
