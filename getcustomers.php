2330507c48f954cc6c6bc3a5e02323ef6bc0152a
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 18:29:48 -0500

    edit

--- getcustomers.php
+++ getcustomers.php
@@ -13,7 +13,8 @@
     echo "<li>";
     echo '<input type="radio" name="customer" value="';
            echo $row["customerID"];
-           echo '">'. $row["firstName"]. " " .$row["lastName"];
+           echo '">'. $row["firstName"]. " " .$row["lastName"] "-";
+
     echo "</li>";
 
 		//echo '<input type = "radio" name = "customername" value = "';
