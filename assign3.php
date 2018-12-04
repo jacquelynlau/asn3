feafedc9d3c8e670766df837d9376777108312c0
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 20:28:45 -0500

    e

--- assign3.php
+++ assign3.php
@@ -20,33 +20,32 @@
 
 </div>
 
-
+	<!-- Q1 -->
   <form action = "getproducts1.php" method = "post">
-
-	<?php
-			include "getcustomers.php";
-	?>
-
-	<input type = "submit" value = "Get Purchases">
-
+		<?php
+				include "getcustomers.php";
+		?>
+				<input type = "submit" value = "Get Purchases">
   </form>
 
-	<form method="post" action="assign3.php">
 
-	<!-- organize by ascending or descending order button -->
-	<select name = "description">
-	  <option selected = "selected" value = "ASC"> Ascending </option>
-	  <option value = "DESC"> Descending </option>
-	</select>
+	<form action="reorderbydescription.php" method="post">
 
-	<!-- organize by price or name button -->
-	<select name = "type">
-	  <option selected = "selected" value = "price"> Price </option>
-	  <option value = "name"> Name </option>
-	</select>
+		<!-- organize by ascending or descending order button -->
+		<select name = "description">
+	  	<option selected = "selected" value = "ASC"> Ascending </option>
+	  	<option value = "DESC"> Descending </option>
+		</select>
 
-	<!-- reorder the products button -->
-	<input name = "submit" type ="submit" value = "Reorder Table"/>
+		<!-- organize by price or name button -->
+		<select name = "type">
+	  	<option selected = "selected" value = "price"> Price </option>
+	  	<option value = "name"> Name </option>
+		</select>
+
+		<!-- reorder the products button -->
+		<input name = "submit" type ="submit" value = "Reorder Table"/>
+	</form>
 
 	<?php
 		include "reorderbydescription.php";
