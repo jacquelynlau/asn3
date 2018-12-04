c23fb9dc21e1ed8f254344b626282aff34893ad5
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 18:14:53 -0500

    Q7

--- assign3.php
+++ assign3.php
@@ -209,12 +209,13 @@ mysqli_close($connection);
 <!-- Q7.  -->
 <h3> 7. List the customers who have bought more than a give quantity: </h3>
 
-<!-- enter the phone number to update -->
-<input name="quantity7" type="text" placeholder="Enter Quantity">
-<br>
+<form action = "Q7.php" method = "post">
+		<!-- enter the phone number to update -->
+		<input name="quantity7" type="text" placeholder="Enter Quantity">
+		<br>
 
-<!-- submit button to update phone number -->
-<input name = "submit7" type = "submit" value = "Find">
+		<!-- submit button to update phone number -->
+		<input name = "submit7" type = "submit" value = "Find">
 </form>
 
 
