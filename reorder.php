4f25fe381e8b381d1d478832c5f83d22055196d5
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 09:11:41 -0500

    d

--- reorder.php
+++ reorder.php
@@ -7,7 +7,7 @@
 <html>
 <body>
 
-<form action="Q2.php" method="post">
+<form action="#" method="post">
 <!-- organize by ascending or descending order button -->
 <select name = "order">
     <option value = "ASC"> Ascending </option>
@@ -25,7 +25,25 @@
 
 </form>
 
-
+<?php
+         # If the user pressed "Show"
+         if (isset($_POST["reorder"])) {
+           # Reorder the products with depending on the user's choice
+           //$query = 'SELECT * FROM Product ORDER BY ' . $_POST["order"] . ' ' . $_POST["ascdesc"];
+           $query = "SELECT * FROM product ORDER BY " . $_POST["type"] . " " . $_POST["order"] ;
+           $result = mysqli_query($connection, $query);
+           if (!$result) {
+             die("Query failed");
+           }
+           # Create a loop to print the data
+           while ($row = mysqli_fetch_assoc($result)) {
+             echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["productDescription"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["numberItems"];
+           }
+           mysqli_free_result($result);
+         }
+         # Close connection after
+         mysqli_close($connection);
+       ?>
 
 
 </html>
