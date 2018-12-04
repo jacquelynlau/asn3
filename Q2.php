643f4f07d7b42d7ec3f46973a3ea1ce3bf25c5c5
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 00:04:42 -0500

    f

--- Q2.php
+++ Q2.php
@@ -12,22 +12,26 @@
         die("databases query failed.");
     }
 
-   echo "</br>So many products</br>";
-   echo "<table align='center'>";
-   echo "<th>Product Name</th>";
-   echo	"<th>Price</th>";			//create a table
-   echo	"<th>Quantity Left</th>";
-   
-   while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . $row['description'] . " </td>";
-	echo "<td>" . $row['cost'] . " </td>"; //place the info into the table
-	echo "<td>" . $row['quantity'] . " </td>";
-	echo "</tr>";
-   }
-   echo "</table>";
-   mysqli_free_result($result);
-?>
+    while ($row = mysqli_fetch_assoc($result)) {
+              echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["productDescription"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["numberItems"];
+            }
 
+  // echo "</br>So many products</br>";
+  // echo "<table align='center'>";
+  // echo "<th>Product Name</th>";
+  // echo	"<th>Price</th>";			//create a table
+  // echo	"<th>Quantity</th>";
+
+   //while ($row = mysqli_fetch_assoc($result)) {
+    //    echo "<tr>";
+    //   echo "<td>" . $row['description'] . " </td>";
+	//echo "<td>" . $row['costPerItem'] . " </td>"; //place the info into the table
+	//echo "<td>" . $row['quantity'] . " </td>";
+	//echo "</tr>";
+
+   //echo "</table>";
+   mysqli_free_result($result);
+
+ mysqli_close($connection);
 
 ?>
