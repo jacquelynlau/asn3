69d994203dbe94b741a706e6a49991b15ec1f92e
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 20:21:50 -0500

    efsdfsd

--- reorderbydescription.php
+++ reorderbydescription.php
@@ -3,7 +3,7 @@
 include 'connecttodb.php';
 
 if (isset($_POST["submit"])) {
-$query = 'SELECT * from products ORDER BY ' . $_POST["type"] . ' ' . $_POST["description"];
+$query = 'SELECT * FROM Product ORDER BY ' . $_POST["type"] . ' ' . $_POST["description"];
 
 $result = mysqli_query($connection, $query);
             if (!$result) {
