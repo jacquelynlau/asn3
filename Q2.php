dc5cf1fc09efd2f42c8136981608aecb481d696f
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 23:09:18 -0500

    f

--- Q2.php
+++ Q2.php
@@ -1,28 +1,26 @@
 <?php
 	include 'connecttodb.php';
 
-  if (isset($_POST["submit"])) {
-  if(isset($_POST["price"]) && isset($_POST["ascending"])){
-   $query = "SELECT * FROM Product ORDER BY " . $_POST["price"] . " " . $_POST["ascending"] ;
-   }
-    $result = mysqli_query($connection,$query);
+    if (isset($_POST["submit"]) && (isset($_POST["ascending"]) && (isset($_POST["price"])) {
 
-    if (!$result) {
-         die("databases query failed.");
-     }
-    echo "</br>So many products</br>";
-    echo "<table align='center'>";
-    echo "<th>Product Name</th>";
-    echo	"<th>Price</th>";			//create a table
-    echo	"<th>Quantity Left</th>";
-    while ($row = mysqli_fetch_assoc($result)) {
-         echo "<tr>";
-         echo "<td>" . $row['description'] . " </td>";
- 	echo "<td>" . $row['cost'] . " </td>"; //place the info into the table
- 	echo "<td>" . $row['quantity'] . " </td>";
- 	echo "</tr>";
-    }
-    echo "</table>";
-    mysqli_free_result($result);
-  }
+             # Reorder the products with depending on the user's choice
+             $query = 'SELECT * from Product ORDER BY ' . $_POST["ascending"] . ' ' . $_POST["price"];
+             $result = mysqli_query($connection, $query);
+             if (!$result) {
+               die("Query failed");
+             }
+             # Create a loop to print the data
+             while ($row = mysqli_fetch_assoc($result)) {
+               echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["description"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["quantity"];
+             }
+             mysqli_free_result($result);
+           }
+           # Close connection after
+           mysqli_close($connection);
+         ?>
+
+
+
+
+
  ?>
