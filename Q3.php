56306f57d1760c2e33ff76fc38cccb0b55008d46
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 11:59:27 -0500

    d

--- Q3.php
+++ Q3.php
@@ -10,4 +10,38 @@
     if (!$productID || !$quantity || !$customerID){
                 echo "<p> There is missing information. </p>";
     }
+
+    $query = 'SELECT COUNT(*) as count, quantityPurchased FROM CustomerPurchases WHERE customerID=' . $customerID . ' AND productID=' . $productID;
+         $result = mysqli_query($connection, $query);
+         # Check if query works
+         if (!$result) {
+           die("Query failed");
+         }
+         $row = mysqli_fetch_assoc($result);
+         # If the customer has not purchased this product yet, then add it onto the purchase table
+         if ($row["count"] != 1) {
+           $insert = "INSERT INTO CustomerPurchases VALUES ('$customerID', '$productID', '$quantity')";
+           $insert_result = mysqli_query($connection, $insert);
+           if (!$insert_result) {
+             die("Insert Query has failed");
+           }
+         }
+         # If the customer has purchased this product already
+         else {
+           $newTotal = $row["quantity"] + $quantity;
+           $add_query = 'UPDATE CustomerPurchases SET quantityPurchased=' . $newTotal . ' WHERE customerid=' . $customerID . ' AND productID=' . $productID;
+           $add_result = mysqli_query($connection, $add_query);
+           if(!$add_result) {
+             die("Add query has failed");
+           }
+         }
+         mysqli_free_result($$result);
+         mysqli_free_result($insert_result);
+         mysqli_free_result($add_result);
+       }
+       # Close connection after
+       mysqli_close($connection);
+     ?>
+
+
 }
