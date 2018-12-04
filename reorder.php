ee357874adda6761c76baf2d7baf0b047c96357f
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 23:51:42 -0500

    f

--- reorder.php
+++ reorder.php
@@ -8,9 +8,21 @@
 <body>
 
 <!-- organize by ascending or descending order button -->
-  <input type = "radio" name = "ascending" value = "ASC"> Ascending </option>
-  <input type = "radio" name = "descending" value = "DESC"> Descending </option>
+<select name = "order">
+    <input type = "radio" name = "ascending" value = "ASC"> Ascending </option>
+    <input type = "radio" name = "descending" value = "DESC"> Descending </option>
+</select>
 
+
+
+  <select name="ordering">
+  	<option selected="selected" value="ASC">Ascending</option>//option ascending and descending table
+  	<option value="DESC">Descending</option>
+  </select>
+  <select name="type">
+          <option selected="selected" value="cost">Price</option>// option price or name
+          <option value="description">Name</option>
+  </select>
 
 <!-- organize by price or name button -->
   <input type = "radio" name = "price" value = "PRICE"> Price </option>
