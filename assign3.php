c8e9755c93db8ee3aa5faf530f2ce78ab54d620d
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 20:11:01 -0500

    hfdf

--- assign3.php
+++ assign3.php
@@ -29,14 +29,36 @@
 	<?php
 			include "getcustomers.php";
 	?>
-	
+
 	<input type = "submit" value = "Get Purchases">
 
   </form>
 
-<?php
-		include "reorderproducts.php";
-?>
+	<form method="post" action="assign3.php">
+
+
+
+	<!-- organize by ascending or descending order button -->
+	<select name = "description">
+	  <option selected = "selected" value = "ASC"> Ascending </option>
+	  <option value = "DESC"> Descending </option>
+	</select>
+
+	<!-- organize by price or name button -->
+	<select name = "type">
+	  <option selected = "selected" value = "price"> Price </option>
+	  <option value = "name"> Name </option>
+	</select>
+
+	<!-- reorder the products button -->
+	<input type ="submit" value = "Reorder Table"/>
+
+	<?php
+	  include 'getproducts2.php';
+	?>
+
+
+
 
 
 </body>
--- reorderproducts.php
+++ reorderproducts.php
@@ -1,30 +1,25 @@
 <!DOCTYPE html>
 
-
+<!--
 <?php
   include 'connecttodb.php';
   include 'reorderbydescription.php';
   include 'reorderbyprice.php'
 ?>
 
-<form method="post" action="assign3.php">
-<!-- organize by ascending or descending order button -->
+<!-- organize by ascending or descending order button
 <select name = "description">
   <option selected = "selected" value = "ASC"> Ascending </option>
   <option value = "DESC"> Descending </option>
-</select>
+</select> -->
 
-<!-- organize by price or name button -->
+<!-- organize by price or name button
 <select name = "type">
   <option selected = "selected" value = "price"> Price </option>
   <option value = "name"> Name </option>
 </select>
 
-<!-- reorder the products button -->
-<input type = "submit" value = "Reorder Table"/>
+<!-- reorder the products button
+<input type ="submit" value = "Reorder Table"/>
 
-<?php
-  include 'getproducts2.php';
-?>
-
-</form>
+<
