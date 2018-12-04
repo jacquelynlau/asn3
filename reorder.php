55615c17cdf5d090c1b63b802affc0bf4126c7a2
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 23:43:08 -0500

    fuckkkkk

--- reorder.php
+++ reorder.php
@@ -8,16 +8,13 @@
 <body>
 
 <!-- organize by ascending or descending order button -->
-<select name= "order">
   <input type = "radio" name = "ascending" value = "ASC"> Ascending </option>
   <input type = "radio" name = "descending" value = "DESC"> Descending </option>
-</select>
+
 
 <!-- organize by price or name button -->
-<select name = "type">
   <input type = "radio" name = "price" value = "PRICE"> Price </option>
   <input type = "radio" name = "name" value = "DESC"> Name </option>
-</select>
 
 <input name = "reorder" type = "submit" value = "Reorder Table">
 
