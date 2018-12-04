9f2301a136a80a7c8a160b6c9eca089b742fffc6
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 20:48:19 -0500

    d

--- assign3.php
+++ assign3.php
@@ -48,8 +48,23 @@
 		<input name = "reorder" type ="submit" value = "Reorder Table"/>
 	</form>
 
-	<?php
-		include "reorderbydescription.php";
+<?php
+	include 'connecttodb.php';
+	if (isset($_POST["reorder"])) {
+	$query = 'SELECT * FROM Product ORDER BY ' . $_POST["description"] . ' ' . $_POST["type"];
+
+	$result = mysqli_query($connection, $query);
+	            if (!$result) {
+	              die("Query failed");
+	            }
+	            # Create a loop to print the data
+	            while ($row = mysqli_fetch_assoc($result)) {
+	              echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["description"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["quantity"];
+	            }
+	            mysqli_free_result($result);
+	          }
+	          # Close connection after
+	          mysqli_close($connection);
 	?>
 
 </body>
