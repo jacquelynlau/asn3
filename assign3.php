fb492a4bfa676425ae494e069cfa78c88dc73584
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 22:46:17 -0500

    j

--- assign3.php
+++ assign3.php
@@ -19,39 +19,22 @@
 </div>
 
 <!-- Q1 -->
-  <form action = "Q1.php" method = "post">
+<form action = "Q1.php" method = "post">
 		<?php
 				include "getcustomers.php";
 		?>
 				<input type = "submit" value = "Get Purchases">
-  </form>
+</form>
 
 <!-- Q2 -->
 <p> Select how you want to order the product: </p>
 
-<form action="Q2.php" method="post">
+<form action= "Q2.php" method = "post">
 	<?php
-		include "reorder.php";
+			include "reorder.php";
 	?>
-<input type = "submit" value = "Reorder Table">
-
+			<input type = "submit" value = "Reorder Table">
 </form>
-
-		<!-- organize by ascending or descending order button -->
-		<select name = "description">
-	  	<option value = "ASC"> Ascending </option>
-	  	<option value = "DESC"> Descending </option>
-		</select>
-
-		<!-- organize by price or name button -->
-		<select name = "type">
-	  	<option value = "price"> Price </option>
-	  	<option value = "name"> Name </option>
-		</select>
-
-		<!-- reorder the products button -->
-		<input name = "reorder" type ="submit" value = "Reorder Table"/>
-	</form>
 
 
 </body>
--- reorder.php
+++ reorder.php
@@ -1,24 +1,19 @@
+<?php
+    include 'connecttodb.php';
+?>
+
 
 <!DOCTYPE html>
 <html>
 <body>
 
 <!-- organize by ascending or descending order button -->
-<select name = "description">
-  <option value = "ASC"> Ascending </option>
-  <option value = "DESC"> Descending </option>
-</select>
+  <input type = "radio" name = "ascending" value = "ASC"> Ascending </option>
+  <input type = "radio" name = "descending" value = "DESC"> Descending </option>
 
 <!-- organize by price or name button -->
-<select name = "type">
-  <option value = "price"> Price </option>
-  <option value = "name"> Name </option>
-</select>
-
-<!-- reorder the products button -->
-<input name = "reorder" type ="submit" value = "Reorder Table"/>
-
-
+  <input type = "radio" name = "price" value = "PRICE"> Price </option>
+  <input type = "radio" name = "name" value = "DESC"> Name </option>
 
 
 </html>
