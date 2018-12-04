94fccb7821695a0674ae3527e81f01cca5247700
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 10:26:48 -0500

    d

--- Q2.php
+++ Q2.php
@@ -2,20 +2,22 @@
     include 'connecttodb.php';
 ?>
 
-
 <!DOCTYPE html>
 <html>
 <body>
 
-
 <?php
 
-           if(isset($_POST['price']) && isset($_POST['ascending'])) {
-        //   if ($priceOrder == "ASC") {
+// order by ascending prices
+if(isset($_POST['price']) && isset($_POST['ascending'])) {
+    $query = "SELECT * FROM Product ORDER BY costPerItem ASC";
+    }
 
-           // order price ascending
-           $query = "SELECT * FROM Product ORDER BY costPerItem ASC";
-         }
+// order by descending prices
+else if(isset($_POST['price']) && isset($_POST['descending'])) {
+    $query = "SELECT * FROM Product ORDER BY costPerItem DESC";
+    }
+
 
            $result = mysqli_query($connection, $query);
            if (!$result) {
@@ -23,7 +25,7 @@
            }
            # Create a loop to print the data
            while ($row = mysqli_fetch_assoc($result)) {
-             echo '<ul>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["description"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["quantity"];
+             echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["description"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["quantity"];
            }
            mysqli_free_result($result);
 
