731b52dd74b8cf74ed1b3808e3dbec0b74bccce4
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 10:31:19 -0500

    d

--- Q2.php
+++ Q2.php
@@ -25,21 +25,22 @@ else if(isset($_POST['name']) && isset($_POST['ascending'])) {
 
 // order by descending names
 else if(isset($_POST['name']) && isset($_POST['descending'])) {
-        $query = "SELECT * FROM Product ORDER BY costPerItem DESC";
+        $query = "SELECT * FROM Product ORDER BY description DESC";
         }
 
+$result = mysqli_query($connection, $query);
 
-           $result = mysqli_query($connection, $query);
-           if (!$result) {
-             die("Query failed");
-           }
-           # Create a loop to print the data
-           while ($row = mysqli_fetch_assoc($result)) {
+if (!$result) {
+        die("Query failed");
+        }
+
+//while loop to print results
+while ($row = mysqli_fetch_assoc($result)) {
              echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["description"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["quantity"];
            }
            mysqli_free_result($result);
 
-         # Close connection after
+         
          mysqli_close($connection);
        ?>
 
