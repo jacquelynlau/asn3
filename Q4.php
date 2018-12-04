ac11e8f8fb2f80aa440301490558e24828df2a70
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 16:42:34 -0500

    dd

--- Q4.php
+++ Q4.php
@@ -22,37 +22,17 @@
     echo "<p> This customer ID is unavailable. </p>";
   }
 
-  else {
-  $customerID = $_POST["customerID"];
-  $customerFName = $_POST["firstname"];
-  $customerLName = $_POST["lastname"];
-  $customerCity = $_POST["city"];
-  $customerPhone = $_POST["phonenumber"];
-
-  $query = "INSERT INTO Customer VALUES ('$customerID', '$customerFName',
-           '$customerLName', '$customerPhone', '$customerCity')";
-
-  $result = mysqli_query($connection,$query);
-
-  if (!$result) {
-     die("databases query failed.");
-  }
-  else {
-    echo "<p> Customer added </p>";
-  }
-}
-
 
   // if the customerID does not exist, add the new customer
-  //else {
-    //  $query = "INSERT INTO Customer (customerID, firstName, lastName, city, phoneNumber) VALUES('" . $_POST['customerID'] . "' , '" . $_POST['firstname'] . "', '" . $_POST['lastname'] . "', '" . $_POST['city'] . "', '" . $_POST['phonenumber'] . "')";
-      //$result = mysqli_query($connection,$query);
-      //if (!$result) {
-        // die("databases query failed.");
-      //}
-      //else {
-      //  echo "<p> Customer added </p>";
-      //}
-   //}
+  else {
+    $query = "INSERT INTO Customer (customerID, firstName, lastName, city, phoneNumber) VALUES('" . $_POST['customerID'] . "' , '" . $_POST['firstname'] . "', '" . $_POST['lastname'] . "', '" . $_POST['city'] . "', '" . $_POST['phonenumber'] . "')";
+    $result = mysqli_query($connection,$query);
+    if (!$result) {
+        die("databases query failed.");
+      }
+      else {
+       echo "<p> Customer added </p>";
+      }
+   }
    mysqli_close($connection);
 ?>
