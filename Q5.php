9497cfc82b99ea02354c5748056142ec7ea68fae
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 17:46:22 -0500

    comments

--- Q5.php
+++ Q5.php
@@ -1,12 +1,20 @@
 <?php
+
    include 'connecttodb.php';
+
+   //update phone number to the one submitted
    $query = "UPDATE Customer SET phoneNumber ='" . $_POST['phonenumber5'] . "' WHERE customerID='" . $_POST['customer5'] . "'";
-   $result = mysqli_query($connection,$query);//update the phone number of existing customer
+   $result = mysqli_query($connection,$query);
+
+   //if unsuccessful, trigger error message
    if (!$result) {
         die("databases query failed.");
-    }
+   }
+
+   //if successful, trigger success message s
     else {
-        echo "<h1> Phone number uccessfully updated! </h1>";
+        echo "<h1> Phone number successfully updated! </h1>";
     }
+    
    mysqli_close($connection);
 ?>
