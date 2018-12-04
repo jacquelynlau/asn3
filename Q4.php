06bbcb376f8a152f91fc0fb993630a6e23e8d95b
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 13:03:00 -0500

    d

--- Q4.php
+++ Q4.php
@@ -2,10 +2,11 @@
 
 	include 'connecttodb.php';
 
-
+  //if there is missing information
   if (!$customerID || !$firstname || !$lastname || !$city){
               echo "<p> There is missing information. </p>";
   }
+
   //initialize checks for if the customerID exists
   $check4 = 0;
   $query = "SELECT customerID FROM Customer";
@@ -26,19 +27,22 @@
     echo "<p> This customer ID is unavailable. </p>";
   }
 
-  //if ($check4 = 0){
+  else if ($check4 = 0){
+    $customerID = $_POST["customerID"]
+    $customerFName = $_POST["firstname"];
+    $customerLName = $_POST["lastname"];
+    $customerCity = $_POST["city"];
+    $query = "INSERT INTO Customer VALUES ('$customerFName', '$customerLName',
+        '$customerCity', '$customerID')";
 
-  //  $customerID = $_POST["customerID"]
-  //  $customerFName = $_POST["firstname"];
-  //  $customerLName = $_POST["lastname"];
-  //  $customerCity = $_POST["city"];
-  //  $query = "INSERT INTO Customer VALUES ('$customerFName', '$customerLName',
-    //      '$customerCity', '$customerID')";
+    $insert_result = mysqli_query($connection, $query);
 
-      $result = mysqli_query($connection,$query);
 
-  if (!$result) {
-     die("databases query failed.");
-  }
+    if (!$insert_result) {
+               die("Query to insert customer failed: ");
+             }
+             else {
+                 echo 'Customer added!';
+             }
 
 ?>
