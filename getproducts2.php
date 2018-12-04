fc67754ca56ce34cddcefe9f36ffb8858b9ee9f0
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-03 19:36:05 -0500

    edits

--- getproducts2.php
+++ getproducts2.php
@@ -1,7 +1,7 @@
 
 
 
-<?php
+//<?php
   //
 
 
@@ -11,21 +11,21 @@
     //  }
     //  $result = mysqli_query($connection,$query);
 
-      if (!$result) {
-          die("databases query failed.");
-      }
+    //  if (!$result) {
+  //        die("databases query failed.");
+  //    }
 
-      echo "<table>";
-      echo "<th>Product Name</th>";
-         echo	"<th>Price</th>";			//create a table
-         echo	"<th>Quantity Left</th>";
-         while ($row = mysqli_fetch_assoc($result)) {
-              echo "<tr>";
-              echo "<td>" . $row['description'] . " </td>";
-      	echo "<td>" . $row['costPerItem'] . " </td>"; //place the info into the table
-      	echo "<td>" . $row['quantity'] . " </td>";
-      	echo "</tr>";
-         }
-         echo "</table>";
-         mysqli_free_result($result);
+      //echo "<table>";
+      //echo "<th>Product Name</th>";
+        // echo	"<th>Price</th>";			//create a table
+         //echo	"<th>Quantity Left</th>";
+         //while ($row = mysqli_fetch_assoc($result)) {
+        //      echo "<tr>";
+        //      echo "<td>" . $row['description'] . " </td>";
+      	//echo "<td>" . $row['costPerItem'] . " </td>"; //place the info into the table
+      	//echo "<td>" . $row['quantity'] . " </td>";
+      //	echo "</tr>";
+      //   }
+      //   echo "</table>";
+      //   mysqli_free_result($result);
   ?>
