e2ba97e82e110d440c06f49f4b8f00e93c4dc51e
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 00:13:28 -0500

    d

--- Q2.php
+++ Q2.php
@@ -16,22 +16,7 @@
               echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["description"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["quantity"];
             }
 
-  // echo "</br>So many products</br>";
-  // echo "<table align='center'>";
-  // echo "<th>Product Name</th>";
-  // echo	"<th>Price</th>";			//create a table
-  // echo	"<th>Quantity</th>";
-
-   //while ($row = mysqli_fetch_assoc($result)) {
-    //    echo "<tr>";
-    //   echo "<td>" . $row['description'] . " </td>";
-	//echo "<td>" . $row['costPerItem'] . " </td>"; //place the info into the table
-	//echo "<td>" . $row['quantity'] . " </td>";
-	//echo "</tr>";
-
-   //echo "</table>";
-   mysqli_free_result($result);
-
- mysqli_close($connection);
+mysqli_free_result($result);
+mysqli_close($connection);
 
 ?>
