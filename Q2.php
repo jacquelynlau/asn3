7eb9dacd33045550c6b4b82fd46e879319f808eb
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 01:08:22 -0500

    d

--- Q2.php
+++ Q2.php
@@ -1,20 +1,25 @@
 <?php
-
-  $query = "SELECT * FROM Product ORDER BY description ASC";
-  if(isset($_POST["type"]) && isset($_POST["order"])){
-    $query = 'SELECT * from products ORDER BY ' . $_POST["type"] . ' ' . $_POST["order"];
-  }
-
+   $query = "SELECT * FROM Product ORDER BY description ASC";//default setting
+   if(isset($_POST["type"]) && isset($_POST["order"])){//if the user chooses, get new ordering
+   $query = "SELECT * FROM product ORDER BY " . $_POST["type"] . " " . $_POST["order"] ;
+   }
    $result = mysqli_query($connection,$query);
 
    if (!$result) {
         die("databases query failed.");
     }
-
-    while ($row = mysqli_fetch_assoc($result)) {
-              echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' . $row["description"] . ', Cost: ' . $row["costPerItem"] . ', Quantity: ' . $row["quantity"];
-            }
-
-mysqli_free_result($result);
-mysqli_close($connection);
+   echo "</br>So many products</br>";
+   echo "<table align='center'>";
+   echo "<th>Product Name</th>";
+   echo	"<th>Price</th>";			//create a table
+   echo	"<th>Quantity Left</th>";
+   while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . $row['description'] . " </td>";
+	echo "<td>" . $row['costPerItem'] . " </td>"; //place the info into the table
+	echo "<td>" . $row['quantity'] . " </td>";
+	echo "</tr>";
+   }
+   echo "</table>";
+   mysqli_free_result($result);
 ?>
