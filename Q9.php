687e843f6850dd734a31ebeec2319c93b349712d
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 23:18:02 -0500

    Q9 IS NOT WORKING!!!!!!

--- Q9.php
+++ Q9.php
@@ -5,39 +5,18 @@
 <h1> Total number of purchases for a particular product: </h1>
 
 <?php
-
-//get total number of purchases for that product selected
-$whichProduct = $_POST["product9"];
-
-
- echo $amount["total"];
-
- if (!$result) {
-                die("databases query failed.");
-            }
-
-
-
-
-
-
-
-
-mysqli_free_result($result);
-
-
-
-mysqli_close($connection);
-
-
-
-
-
-
-
-
-
- ?>
+   include 'connectdb.php';
+   $query = "SELECT description, cost, SUM(PurchaseS.quantity) as sales FROM Purchases JOIN Product ON Purchases.productID=Product.productID WHERE Purchases.productID='" . $_POST["prod"] ."'";
+   $result = mysqli_query($connection,$query);
+   if (!$result) {
+        die("databases query failed.");
+    }
+   while ($row = mysqli_fetch_assoc($result)) {
+        echo $row["description"] . " sales: $" . $row["sales"] * $row["cost"];
+   }
+   mysqli_free_result($result);
+   mysqli_close($connection);
+?>
 
 
 
