33b6c1e7b6e0513a6e8b7d4e323e0040de907176
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 22:03:58 -0500

    s

--- Q9.php
+++ Q9.php
@@ -8,19 +8,18 @@
 
 //get total number of purchases for that product selected
 $whichProduct = $_POST["product9"];
- $query = 'SELECT SUM(quantity) FROM CustomerPurchases WHERE Purchases.productID = (SELECT Product.productID FROM Product WHERE Product.description="' . $whichProduct . '")';
 
- $result = mysqli_query($connection,$query);
+
+ $query = 'SELECT SUM(quantity) as total FROM Purchase WHERE productID=' . $whichProduct . ' GROUP BY productID';
+ //$query = 'SELECT SUM(quantity) FROM CustomerPurchases WHERE Purchases.productID = (SELECT Product.productID FROM Product WHERE Product.description="' . $whichProduct . '")';
+ $result = mysqli_query($connection, $query);
+ $amount = mysqli_fetch_assoc($result);
+
+ echo $amount["total"];
 
  if (!$result) {
                 die("databases query failed.");
             }
-
-$row = mysqli_fetch_assoc($result);
-                     echo '<p>total quantity of products purchased ';
-                     echo $row["SUM(quantity)"];
-                     echo '</p>';
-
 
 
 
