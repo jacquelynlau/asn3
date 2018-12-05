59310bebfc61a14eb3f4b1c9a8edad0cdea5878d
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 22:41:15 -0500

    Q7

--- Q7.php
+++ Q7.php
@@ -1,11 +1,13 @@
 <?php
 
-      include 'connecttodb.php';
+    include 'connecttodb.php';
 
         if (isset($_POST["submit7"])) {
-          # Put it in variables
+
+
+
           $quantity = $_POST["quantity"];
-          $amount = intval($quantity);
+        //  $amount = intval($quantity);
 
           # Query to get the customer and product description
           $query = 'SELECT firstName, lastName, description, quantity FROM Purchases INNER JOIN Product ON Product.productID=Purchases.productID INNER JOIN Customer ON
@@ -15,11 +17,16 @@
           if (!$result) {
               die("databases query failed.");
           }
+
+
+          echo "<ul>";
           while ($row = mysqli_fetch_assoc($result)) {
             echo '<li><b>' . $row["firstName"] . ' ' . $row["lastName"] . '</b> Purchased <b>' . $row["description"] . '</b>. Amount: <b>' . $row["quantity"] . '</b></li>';
           }
+          echo "</ul>";
           mysqli_free_result($result);
         }
-        # Close database
+
+        
         mysqli_close($connection);
       ?>
