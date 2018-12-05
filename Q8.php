42b2a850fb84b156cc59cd0ae59bb80a7cd97cb2
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 21:12:24 -0500

    q8

---
+++ Q8.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  include 'connecttodb.php';
+
+  //get the products that have not been purchased
+  $query = 'SELECT * FROM Product WHERE productID NOT IN (SELECT productID FROM Purchases)';
+  $result = mysqli_query($connection, $query);
+
+  //if unsuccessful, trigger error message
+  if (!$result) {
+          die("Query failed");
+  }
+
+  //show list of products that have not been purchased
+  while ($row = mysqli_fetch_assoc($result)) {
+            echo '<li>' . 'Product ID: ' . $row["productID"] . ', Name: ' .
+            $row["description"] . ', Cost: ' . $row["costPerItem"];
+  }
+
+  mysqli_close($connection);
+
+?>
+
+
+
+
+
+</body>
+</html>
