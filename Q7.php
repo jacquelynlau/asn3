50879ed5beb4d12801dd131b5744d2028802d631
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 22:35:56 -0500

    Q7

--- Q7.php
+++ Q7.php
@@ -1,23 +1,23 @@
 <?php
-   include 'connecttodb.php';
 
-   $quantity= $_POST["quantity7"];
-   $amount = intval($quantity);
+        if (isset($_POST["submit7"])) {
+          # Put it in variables
+          $quantity = $_POST["quantity"];
+          $amount = intval($quantity);
 
-   $query = 'SELECT firstName, lastName, description, quantityPurchased FROM CustomerPurchases INNER JOIN Product ON Product.productID = Purchases.productID INNER JOIN Customer ON
-           Customers.customerID=Purchases.customerID WHERE quantity>=' . $amount;
-
-   $result = mysqli_query($connection, $query);
-
-   if (!$result) {
-             die("databases query failed.");
-   }
-   while ($row = mysqli_fetch_assoc($result)) {
-           echo '<li><b>' . $row["firstName"] . ' ' . $row["lastName"] . '</b> Purchased <b>' . $row["description"] . '</b>. Amount: <b>' . $row["quantity"] . '</b></li>';
-         }
-  mysqli_free_result($result);
-
-
-mysqli_close($connection);
-
-?>
+          # Query to get the customer and product description
+          $query = 'SELECT firstName, lastName, description, quantity FROM Purchases INNER JOIN Product ON Product.productID=Purchases.productID INNER JOIN Customer ON
+          Customer.customerID=Purchases.customerID WHERE quantity>=' . $amount;
+          $result = mysqli_query($connection, $query);
+          # Check if it worked
+          if (!$result) {
+              die("databases query failed.");
+          }
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo '<li><b>' . $row["firstName"] . ' ' . $row["lastName"] . '</b> Purchased <b>' . $row["description"] . '</b>. Amount: <b>' . $row["quantity"] . '</b></li>';
+          }
+          mysqli_free_result($result);
+        }
+        # Close database
+        mysqli_close($connection);
+      ?>
