653070b6612fa42761728867bd48df846622c2c4
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 21:51:33 -0500

    d

--- Q9.php
+++ Q9.php
@@ -8,16 +8,10 @@
 
 $whichProduct = $_POST["product9"];
 
-//get total number of purchases for that product selected
-$query = 'SELECT SUM(quantityPurchased) as total FROM Purchases WHERE productID=' . $whichProduct . ' GROUP BY productID';
-$result = mysqli_query($connection,$query);
+echo $whichProduct;
 
-if (!$result) {
-        die("databases query failed.");
-    }
-   while ($row = mysqli_fetch_assoc($result)) {//display total sales
-        echo $row["description"] . " sales: $" . $row["quantityPurchased"] * $row["costPerItem"];
-   }
+
+//get total number of purchases for that product selected
    mysqli_free_result($result);
    mysqli_close($connection);
 
