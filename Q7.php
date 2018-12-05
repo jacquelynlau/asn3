6557ae3711f70b8d3b004e2686c25ad1f28da55e
Jacquelyn Lau <jlau333@uwo.ca>
2018-12-04 22:47:09 -0500

    Q7 EDITS

--- Q7.php
+++ Q7.php
@@ -4,27 +4,27 @@
 
     $quantity = $_POST["quantity"];
 
-        $amount = intval($quantity);
+    //get the integer value of a variab
+    $amount = intval($quantity);
 
-          # Query to get the customer and product description
-          $query = 'SELECT firstName, lastName, description, quantity FROM Purchases INNER JOIN Product ON Product.productID=Purchases.productID INNER JOIN Customer ON
-          Customer.customerID=Purchases.customerID WHERE quantity>=' . $amount;
+    //get customer and product description that is over the specific quantity entered
+    $query = 'SELECT firstName, lastName, description, quantity FROM Purchases INNER JOIN Product ON Product.productID=Purchases.productID INNER JOIN Customer ON
+    Customer.customerID=Purchases.customerID WHERE quantity>=' . $amount;
 
-          $result = mysqli_query($connection, $query);
+    $result = mysqli_query($connection, $query);
 
-          if (!$result) {
+    //if unsuccessful, trigger error message
+    if (!$result) {
               die("databases query failed.");
-          }
+    }
 
-
-          echo "<ul>";
+    //prints out the list
+    echo "<ul>";
           while ($row = mysqli_fetch_assoc($result)) {
             echo '<li><b>' . $row["firstName"] . ' ' . $row["lastName"] . '</b> Purchased <b>' . $row["description"] . '</b>. Amount: <b>' . $row["quantity"] . '</b></li>';
           }
-          echo "</ul>";
-          mysqli_free_result($result);
+    echo "</ul>";
 
-
-
-        mysqli_close($connection);
-      ?>
+   mysqli_free_result($result);
+   mysqli_close($connection);
+?>
